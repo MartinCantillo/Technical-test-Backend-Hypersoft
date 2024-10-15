25cc88a5378ed162ee40c5a3268921f9c4a6ce60
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-10-15 13:46:29 -0500

    Rest api ready without Spring security

--- src/main/java/com/test/backend/Rest/ProductApi.java
+++ src/main/java/com/test/backend/Rest/ProductApi.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -24,8 +26,37 @@ public class ProductApi {
     public String helloWord(){
         return "Hello World";
     }
-    @PostMapping
+    @PostMapping("/addProduct")
     public ResponseEntity<String> addProduct(@RequestBody Products product) {
+
+        if (product.getName().trim().isEmpty() ||product.getPrice().compareTo(BigDecimal.ZERO) < 0
+        || product.getQuantity() < 0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Por favor verifica .");
+        }
+
+        boolean result = productService.addProduct(product);
+        if (result) {
+            return ResponseEntity.status(HttpStatus.CREATED).body("Producto creado correctamente.");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al crear el producto.");
+        }
+    }
+    @GetMapping("getAll")
+    public ResponseEntity<List<Products>> getAllProducts() {
+        List<Products> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<Object> getProductById(@PathVariable long id) {
+        Optional<Products> product = productService.getProductById(id);
+        return product.<ResponseEntity<Object>>map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("El producto no existe"));
+
+    }
+
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable long id, @RequestBody Products product) {
 
         if (product.getName().trim().isEmpty()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("El nombre no puede estar vacío.");
@@ -37,11 +68,22 @@ public class ProductApi {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("La cantidad no puede ser negativa.");
         }
 
-        boolean result = productService.addProduct(product);
-        if (result) {
-            return ResponseEntity.status(HttpStatus.CREATED).body("Producto creado correctamente.");
+
+        boolean updated = productService.updateProduct(id, product);
+        if (updated) {
+            return ResponseEntity.ok("Producto actualizado correctamente.");
         } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al crear el producto.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Producto no encontrado.");
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable long id) {
+        boolean deleted = productService.deleteProduct(id);
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 
